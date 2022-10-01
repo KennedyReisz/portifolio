400f96b97bf0ad171757c7feedce9dd7db5ef7d5
Kennedy <zelpzxhacker@gmail.com>
2022-09-30 22:57:14 -0300

    Setuping Home

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,12 +1,30 @@
+// Import's
+import Link from "next/link";
+
 // Asset's
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.headerContainer}>
-      
+      <h1 className={styles.headerTitle}>Kennedy</h1>
+
+      <nav className={styles.navbar}>
+        <Link href={""}>
+          <a className={styles.navItem}>About</a>
+        </Link>
+        <Link href={""}>
+          <a className={styles.navItem}>Skills</a>
+        </Link>
+        <Link href={""}>
+          <a className={styles.navItem}>Projects</a>
+        </Link>
+        <Link href={""}>
+          <a className={`${styles.navItem} ${styles.lastItem}`}>Contact</a>
+        </Link>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,13 +1,20 @@
 // Import's
-import Header from "../header"
+import Header from "../header";
 
 // Asset's
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <Header />
-  )
-}
+    <>
+      <Header />
+      
+      <div className={styles.home}>
+        <h1 className={styles.mainPhrase}>I<span>&#180;</span>m Kennedy<span>!</span></h1>
+        <p className={styles.subPhrase}>Full-Stack Developer<span>.</span></p>
+      </div>
+    </>
+  );
+};
 
 export default Home;
