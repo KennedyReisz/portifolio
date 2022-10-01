a8a82c9db0525ba398221318080f05ed28f22050
Kennedy <zelpzxhacker@gmail.com>
2022-10-01 16:47:58 -0300

    Initializing Project

---
+++ components/projects/index.tsx
@@ -0,0 +1,14 @@
+// Import's
+import Image from "next/image";
+import Link from "next/link";
+
+// Asset's
+import styles from "./Projects.module.css";
+
+const Projects = () => {
+  return (
+    <div>213123</div>
+  )
+}
+
+export default Projects;
