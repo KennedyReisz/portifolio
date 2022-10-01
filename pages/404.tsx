2b4596083466b6b09aaf618880d87e3d1d1ac594
Kennedy <zelpzxhacker@gmail.com>
2022-10-01 15:19:18 -0300

    Fixed 404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,13 +4,12 @@ import styles from "../styles/404.module.css";
 const NotFound = () => {
   setTimeout(() => {
     window.location.href = "/"
-  }, 4000)
+  }, 5000)
 
   return (
     <div className={styles.notFound}>
       <h1>Ops...<br /></h1>
       <h1>Page Not Found! 404.</h1>
-
       <p>wait redirecting you to Home page...</p>
     </div>
   );
