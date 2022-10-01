3b0077611ab410907598787ae56cf8900181ce55
Kennedy <zelpzxhacker@gmail.com>
2022-10-01 09:36:28 -0300

    Finished About

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,12 +1,42 @@
 // Import's
+import Image from "next/image";
 
 // Asset's
 import styles from "./About.module.css";
+import aboutImage from "../../assets/aboutImage.png";
+import aboutBorder from "../../assets/aboutBorder.svg";
 
 const About = () => {
   return (
-    <h1>top</h1>
-  )
-}
+    <section className={styles.about}>
+      <div className={styles.aboutContent}>
+        <div className={styles.leftSide}>
+          <div className={styles.aboutImage}>
+            <Image alt="" width={525} height={380} src={aboutImage} />
+          </div>
+        </div>
+        <div className={styles.rightSide}>
+          <h2>ABOUT ME</h2>
+          <h1>
+            I<span>&apos;</span>m Kennedy
+          </h1>
+
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts. Separated they
+            live in Bookmarksgrove right at the coast of the Semantics, a large
+            language ocean.
+          </p>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts. Separated they
+            live in Bookmarksgrove right at the coast of the Semantics, a large
+            language ocean.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
